22fd8537d6556cf0d40f68df21e6c5310b05f38c
doris <dorisdearca02@gmail.com>
2024-10-18 17:04:00 +0200

    modify

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -3,22 +3,6 @@ import numpy as np
 
 
 def load_data(filename):
-    """
-    Load data from a csv file
-
-    Parameters
-    ----------
-    filename : string
-        Filename to be loaded.
-
-    Returns
-    -------
-    X : ndarray
-        the data matrix.
-
-    y : ndarray
-        the labels of each sample.
-    """
     data = read_csv(filename)
     z = np.array(data)
     y = z[:, 0]
